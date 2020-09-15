d95183faca659a92fc7c3564db8ad360f30ce704
Morphix <pmjakubiak13@gmail.com>
2020-09-15 13:41:16 +0200

    Added new solution in go to Length of Last Word problem.

--- Solutions/Length_of_Last_Word/Length_of_Last_Word.go
+++ Solutions/Length_of_Last_Word/Length_of_Last_Word.go
@@ -15,3 +15,15 @@ func lengthOfLastWord(s string) int {
 	}
 	return res
 }
+
+//Another
+func lengthOfLastWord(s string) int {
+    s = strings.TrimSpace(s)
+    ls := len(s)
+    for i:=ls-1;i>=0;i-- {
+        if s[i]==' ' {
+            return ls-i-1
+        }
+    }
+    return ls
+}
